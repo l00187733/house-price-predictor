6d6a8715450060d5762215a9a9fe37c71f983045
l00187733 <l00187733@atu.ie>
2025-08-21 22:48:56 +0000

    created files

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def predict():
     return render_template("result.html", prediction=round(prediction, 2))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
